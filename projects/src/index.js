64d5c948b27caf669c8ddc3c5e7062ab736aa856
Julia Trifonova <yulia3fonova@gmail.com>
2020-11-13 18:51:01 +0300

    homework_1 is done

--- projects/src/index.js
+++ projects/src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(a) {
+  return a;
 }
 
 /*
@@ -29,7 +30,8 @@ function returnFirstArgument() {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
+function sumWithDefaults(a, b=100) {
+  return a + b;
 }
 
 /*
@@ -41,6 +43,7 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+  return fn();
 }
 
 /*
@@ -56,7 +59,10 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {
+var f = returnCounter(10);
+
+function returnCounter(number = 0) {
+  return () => number++;
 }
 
 /*
@@ -69,6 +75,13 @@ function returnCounter(number) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
+  var result = [];
+
+  for (var i = 0; i < arguments.length; i++) {
+    result[i] = arguments[i];
+  }
+
+  return result;
 }
 
 /*
@@ -86,7 +99,8 @@ function returnArgumentsArray() {
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {
+function bindFunction(fn, ...args) {
+  return () => fn(...args);
 }
 
 export {
