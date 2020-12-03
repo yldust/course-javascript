9434bca8fcf1078780a1b93e8d662938a0494338
Julia Trifonova <yldust@yandex.ru>
2020-12-03 18:30:41 +0300

    homework_6 is done

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,10 +19,79 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+let offset = [0, 0];
+let counter = 0;
+let isDown = false;
+let currentDiv = null;
 
-export function createDiv() {}
+document.addEventListener(
+  'mouseup',
+  (e) => {
+    isDown = false;
+  },
+  true
+);
+
+document.addEventListener('mousemove', (e) => {
+  e.preventDefault();
+  if (isDown) {
+    currentDiv.style.left = e.clientX + offset[0] + 'px';
+    currentDiv.style.top = e.clientY + offset[1] + 'px';
+  }
+});
+
+export function createDiv() {
+  const maxWidth = window.innerWidth;
+  const maxHeight = window.innerHeight;
+  const width = randomInteger(1, Math.floor(maxWidth / 4));
+  const height = randomInteger(1, Math.floor(maxHeight / 4));
+  const position = 'absolute';
+  const offsetLeft = randomInteger(0, maxWidth - width);
+  const offsetRight = randomInteger(0, maxHeight - height);
+  const background = randColor();
+  const newDiv = document.createElement('div');
+
+  newDiv.style.background = background;
+  newDiv.style.width = width + 'px';
+  newDiv.style.height = height + 'px';
+  newDiv.style.position = position;
+  newDiv.style.left = offsetLeft + 'px';
+  newDiv.style.top = offsetRight + 'px';
+  newDiv.style.top = '0';
+
+  return newDiv;
+}
+
+function randColor() {
+  let color = '#';
+
+  for (let i = 0; i < 6; i++) {
+    color += randomInteger(0, 16).toString(16);
+  }
+  console.log(color);
+  return color;
+}
+
+function randomInteger(min, max) {
+  const rand = min + Math.random() * (max - min);
+  return Math.floor(rand);
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
-addDivButton.addEventListener('click', function () {});
+addDivButton.addEventListener('click', function () {
+  const div = createDiv();
+
+  div.addEventListener(
+    'mousedown',
+    function (e) {
+      isDown = true;
+      div.style.zIndex = counter++;
+      offset = [div.offsetLeft - e.clientX, div.offsetTop - e.clientY];
+      currentDiv = div;
+    },
+    true
+  );
+
+  homeworkContainer.append(div);
+});
